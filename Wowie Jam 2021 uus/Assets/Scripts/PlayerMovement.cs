9e127e97fccd07d4658116f279c2c9924f21c37e
TrueTipu <59774591+TrueTipu@users.noreply.github.com>
2021-02-26 18:00:37 +0200

    hahmno

---
+++ Wowie Jam 2021 uus/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    bool movable = true;
+    public float tileLength;
+
+    public float waitTime;
+    float waitTimer;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(movable)
+        {
+            if(Input.GetAxisRaw("Horizontal") != 0 )
+            {
+                Move(Mathf.Round(Input.GetAxisRaw("Horizontal")), true);
+            }
+            else if(Input.GetAxisRaw("Vertical") != 0)
+            {
+                Move(Mathf.Round(Input.GetAxisRaw("Vertical")), false);
+            }
+        }
+        else
+        {
+            TileTimer();
+        }
+    }
+
+    void Move(float dir, bool x)
+    {
+        movable = false;
+        waitTimer = waitTime;
+        if(x)
+        {
+            transform.position += new Vector3(dir*tileLength, 0, 0);
+        }
+        else
+        {
+            transform.position += new Vector3(0, dir*tileLength, 0);
+        }
+    }
+    void TileTimer()
+    {
+        waitTimer -= Time.deltaTime;
+        if(waitTimer < 0)
+        {
+            movable = true;
+        }
+    }
+}
