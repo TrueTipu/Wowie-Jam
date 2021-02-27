a031106bf78367b483a787cda772c5e2eccf0938
TrueTipu <59774591+TrueTipu@users.noreply.github.com>
2021-02-27 18:41:53 +0200

    box script

--- Wowie Jam 2021 uus/Assets/Scripts/Box.cs
+++ Wowie Jam 2021 uus/Assets/Scripts/Box.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Transform player;
+    int dir;
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
+        Debug.Log("1");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("2");
+            player = other.transform.parent;
+            if ((transform.position.y - player.transform.position.y) < 0)
+            {
+                dir = -1;
+            }
+            else
+            {
+                dir = 1;
+            }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            transform.position += new Vector3(0, dir * 1, 0);
+        }
     }
 }
