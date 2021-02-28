562f6dba9c2ee23c1aa5b2373ca6f3293fca9a5e
TrueTipu <59774591+TrueTipu@users.noreply.github.com>
2021-02-28 12:51:07 +0200

    endscene + boxi wörkkii

--- Wowie Jam 2021 uus/Assets/Scripts/Box.cs
+++ Wowie Jam 2021 uus/Assets/Scripts/Box.cs
@@ -8,6 +8,8 @@ public class Box : MonoBehaviour
     int dir;
     int x;
     int y;
+
+    public LayerMask interactable;
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("1");
@@ -21,7 +23,6 @@ public class Box : MonoBehaviour
             }
             else if (player.GetComponent<PlayerMovement>().currentParts > 1)
             {
-
 
                 if ((player.GetComponent<PlayerMovement>().oldPos.y - player.transform.position.y) < 0)
                 {
@@ -47,13 +48,36 @@ public class Box : MonoBehaviour
                     y = 0;
                     x = 1;
                 }
-
-                transform.position += new Vector3(dir * x, dir * y, 0);
+                RaycastHit2D hit;
+                hit = new RaycastHit2D();
+                if (x == 1)
+                {
+                    Debug.Log("x");
+                    hit = Physics2D.Raycast(transform.position, dir * Vector2.right, 1f, interactable);
+                }
+                else if (y == 1)
+                {
+                    Debug.Log("y");
+                    hit = Physics2D.Raycast(transform.position, dir * Vector2.up, 1, interactable);
+                }
+                
+                if (hit.collider == null)
+                {
+                    transform.position += new Vector3(dir * x, dir * y, 0);
+                }
+                else
+                {
+                    NotMovable(player);
+                }
             }
             else
             {
-                player.position = player.GetComponent<PlayerMovement>().oldPos;
+                NotMovable(player);
             }
         }
+    }
+    void NotMovable(Transform chara)
+    {
+        chara.position = chara.GetComponent<PlayerMovement>().oldPos;
     }
 }
