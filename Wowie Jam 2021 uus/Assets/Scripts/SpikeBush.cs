dc9a78ac768c2380c0b8653ea392d1eaabf7258e
TrueTipu <59774591+TrueTipu@users.noreply.github.com>
2021-02-28 13:13:30 +0200

    spikebush muutokset jA TP prefab

--- Wowie Jam 2021 uus/Assets/Scripts/SpikeBush.cs
+++ Wowie Jam 2021 uus/Assets/Scripts/SpikeBush.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class SpikeBush : MonoBehaviour
 {
     Transform player;
-    private void OnTriggerEnter2D(Collider2D other)
+
+    public float time;
+    float timer;
+
+    private void Start()
+    {
+        timer = time;
+    }
+    private void OnTriggerStay2D(Collider2D other)
     {
         Debug.Log("1");
         if (other.CompareTag("Player"))
         {
-            FindObjectOfType<AudioManager>().GetComponent<AudioManager>().Play("Kaktus");
-            Debug.Log("2");
-            player = other.transform.GetComponent<Transform>();
-            player.SetParent(null);
-            player.gameObject.layer = 8;
+            if (timer <= 0)
+            {
+                FindObjectOfType<AudioManager>().GetComponent<AudioManager>().Play("Kaktus");
+                Debug.Log("2");
+                player = other.transform.GetComponent<Transform>();
+                player.SetParent(null);
+                player.gameObject.layer = 8;
+                player.tag = "Untagged";
+                timer = time;
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            timer = time;
         }
     }
 }
